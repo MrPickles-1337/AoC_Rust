0a8aefe4f5621024c2b65769284b054a160f0a9e
Kirill Popov <k.popov@hunterhelp.ru>
2023-01-18 12:58:59 +0300

    day5 complete

--- src/day5.rs
+++ src/day5.rs
@@ -16,34 +16,12 @@ pub struct Operation {
 
 impl Operation {
     fn from(input: &str) -> Operation {
-        let mut count = 0;
-        let mut from = 0;
-        let mut to = 0;
+        let spl: Vec<&str> = input.split(" ").collect();
+        let count = spl[1].parse().unwrap();
+        let from = spl[3].parse().unwrap();
+        let to = spl[5].parse().unwrap();
 
-        let mut got_values = 0;
-        let mut iter = input.chars();
-        while let Some(c) = iter.next() {
-            let c = c.to_digit(10);
-            match c {
-                None => continue,
-                _ => (),
-            }
-            if got_values == 0 {
-                count = c.unwrap() as usize;
-                got_values = 1;
-            } else if got_values == 1 {
-                from = c.unwrap() as usize - 1;
-                got_values = 2;
-            } else {
-                to = c.unwrap() as usize - 1;
-            }
-        }
-
-        return Operation {
-            count: count,
-            from: from,
-            to: to,
-        };
+        return Operation { count, from, to };
     }
 }
 
@@ -51,7 +29,7 @@ impl Operation {
 pub fn input_generator(input: &str) -> (Vec<Vec<Container>>, Vec<Operation>) {
     let mut field_result: Vec<Vec<Container>> = Vec::new();
     let mut operations: Vec<Operation> = Vec::new();
-    let (field, operations_data) = input.split_once("\n\r\n").unwrap();
+    let (field, operations_data) = input.split_once("\n\n").unwrap();
     let mut field_lines = field.lines().collect::<Vec<&str>>();
     let last_str = field_lines.last().unwrap().trim();
     field_lines.reverse();
@@ -84,9 +62,8 @@ pub fn part1(input: &(Vec<Vec<Container>>, Vec<Operation>)) -> String {
 
     for op in operations {
         for _ in 0..op.count {
-            dbg!(&field);
-            let con = &field.get_mut(op.from).unwrap().pop().unwrap();
-            field.get_mut(op.to).unwrap().push(*con);
+            let con = &field.get_mut(op.from - 1).unwrap().pop().unwrap();
+            field.get_mut(op.to - 1).unwrap().push(*con);
         }
     }
     let mut result = String::new();
@@ -97,8 +74,24 @@ pub fn part1(input: &(Vec<Vec<Container>>, Vec<Operation>)) -> String {
 }
 
 #[aoc(day5, part2)]
-pub fn part2(_input: &(Vec<Vec<Container>>, Vec<Operation>)) -> &'static str {
-    ""
+pub fn part2(input: &(Vec<Vec<Container>>, Vec<Operation>)) -> String {
+    let field = &mut input.0.clone();
+    let operations = &input.1;
+
+    for op in operations {
+        let i = field.get(op.from -1).unwrap().len() - op.count;
+        let mut drain = field
+            .get_mut(op.from - 1)
+            .unwrap()
+            .drain(i..)
+            .collect::<Vec<Container>>();
+        field.get_mut(op.to - 1).unwrap().append(&mut drain);
+    }
+    let mut result = String::new();
+    for column in field {
+        result.push(column.last().unwrap().0);
+    }
+    return result;
 }
 
 #[cfg(test)]
@@ -110,7 +103,7 @@ mod test {
         let input = "    [D]    
 [N] [C]    
 [Z] [M] [P]
-1   2   3 \n\r\nmove 1 from 2 to 1
+1   2   3 \n\nmove 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2";
@@ -118,5 +111,14 @@ move 1 from 1 to 2";
     }
 
     #[test]
-    fn part2_test() {}
+    fn part2_test() {
+        let input = "    [D]    
+[N] [C]    
+[Z] [M] [P]
+1   2   3 \n\nmove 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+        assert_eq!("MCD", part2(&input_generator(input)));
+    }
 }
