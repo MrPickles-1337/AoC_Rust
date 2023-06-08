67c106fe7a0e2866e4608b3f9242c9f820e9fa1d
Kirill Popov <k.popov@hunterhelp.ru>
2023-06-08 10:52:36 +0300

    uncomment day3 p2

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 const START_LOWER: u8 = b'a' - 1;
 const START_UPPER: u8 = b'A' - 1;
@@ -29,33 +29,33 @@ pub fn part1(input: &str) -> usize {
         .sum::<usize>()
 }
 
-// #[aoc(day3, part2)]
-// pub fn part2(input: &str) -> usize {
-// input
-//     .lines()
-//     .array_chunks::<3>()
-//     .flat_map(|line| {
-//         return line
-//             .iter()
-//             .flat_map(|line| line.chars().collect::<HashSet<_>>().into_iter())
-//             .fold(HashMap::new(), |mut map: HashMap<char, u32>, c| {
-//                 *map.entry(c).or_insert(0) += 1;
-//                 map
-//             })
-//             .into_iter()
-//             .filter(|(_, v)| *v == 3);
-//     })
-//     .map(|c| c.0)
-//     .map(|c| {
-//         let value = if c.is_ascii_lowercase() {
-//             c as u8 - START_LOWER
-//         } else {
-//             c as u8 - START_UPPER + 26
-//         } as usize;
-//         return value;
-//     })
-//     .sum::<usize>()
-// }
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> usize {
+    input
+        .lines()
+        .array_chunks::<3>()
+        .flat_map(|line| {
+            return line
+                .iter()
+                .flat_map(|line| line.chars().collect::<HashSet<_>>().into_iter())
+                .fold(HashMap::new(), |mut map: HashMap<char, u32>, c| {
+                    *map.entry(c).or_insert(0) += 1;
+                    map
+                })
+                .into_iter()
+                .filter(|(_, v)| *v == 3);
+        })
+        .map(|c| c.0)
+        .map(|c| {
+            let value = if c.is_ascii_lowercase() {
+                c as u8 - START_LOWER
+            } else {
+                c as u8 - START_UPPER + 26
+            } as usize;
+            return value;
+        })
+        .sum::<usize>()
+}
 
 #[cfg(test)]
 mod test {
@@ -73,5 +73,13 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     }
 
     #[test]
-    fn part2_test() {}
+    fn part2_test() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+        assert_eq!(70, part2(input));
+    }
 }
