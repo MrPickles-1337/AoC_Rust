1befc5fbb1e28e4a3bea6a7354d1d6dc86835ce5
Kirill Popov <k.popov@hunterhelp.ru>
2023-06-08 10:52:36 +0300

    uncomment day3 p2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// #![feature(iter_array_chunks)]
+#![feature(iter_array_chunks)]
 extern crate aoc_runner;
 
 #[macro_use]
@@ -6,12 +6,13 @@ extern crate aoc_runner_derive;
 extern crate crypto;
 
 pub mod day1;
+pub mod day10;
 pub mod day2;
 pub mod day3;
 pub mod day4;
 pub mod day5;
 pub mod day6;
 pub mod day7;
-pub mod day10;
+pub mod day8;
 
 aoc_lib! { year = 2022 }
