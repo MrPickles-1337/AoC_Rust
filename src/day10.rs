8bcccc87afaddd67b0613854b8eb6b63bb34b820
Kirill Popov <k.popov@hunterhelp.ru>
2023-06-09 17:42:39 +0300

    d10 wip

--- src/day10.rs
+++ src/day10.rs
@@ -1,10 +1,11 @@
+#[derive(Clone, Copy)]
 pub enum Instruction {
     Addx(isize),
     Noop,
 }
 
 #[aoc_generator(day10)]
-pub fn input_generatr(input: &str) -> Vec<Instruction> {
+pub fn input_generator(input: &str) -> Vec<Instruction> {
     input
         .lines()
         .map(|line| {
@@ -50,20 +51,45 @@ pub fn part1(input: &Vec<Instruction>) -> isize {
     res
 }
 
+#[aoc(day10, part2)]
 pub fn part2(input: &Vec<Instruction>) -> usize {
-    let mut screen = vec![false; 6 * 40];
-    let mut position = 0;
+    let mut input = input.clone();
 
-    for instr in input {
+    let mut instructions = input.iter_mut();
+    let mut screen = vec!['.'; 6 * 40];
+    let mut sprite_pos: isize = 0;
+    let mut add: Option<isize> = None;
+    let mut wait = false;
+
+    for (i, pixel) in screen.iter_mut().enumerate() {
+        // println!("{}", (i / 40) as isize * 40);
+        if sprite_pos == i as isize - 1 || sprite_pos == i as isize || sprite_pos + 1 == i as isize
+        {
+            *pixel = '#';
+        }
+        if wait {
+            wait = false;
+            continue;
+        } else if let Some(n) = add {
+            sprite_pos += n;
+            add = None;
+            continue;
+        }
+        let instr = instructions.next().unwrap();
         match instr {
             Instruction::Addx(n) => {
-                todo!();
+                add = Some(*n);
+                wait = true;
             }
-            Instruction::Noop => todo!(),
+            Instruction::Noop => continue,
         }
     }
 
-    todo!()
+    for i in screen.chunks(40) {
+        println!("{}", i.iter().collect::<String>());
+    }
+
+    1
 }
 
 #[cfg(test)]
@@ -218,9 +244,159 @@ addx -11
 noop
 noop
 noop";
-        assert_eq!(13140, part1(&input_generatr(input)));
+        assert_eq!(13140, part1(&input_generator(input)));
     }
 
     #[test]
-    fn part2_test() {}
+    fn part2_test() {
+        let input = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+        part2(&input_generator(input));
+        // uncomment to see test output
+        panic!()
+    }
 }
