ab1922626b1fbc9782aaf01bfe1b2e9ae364a211
MrPickles <kirill2013popov@gmail.com>
2023-12-01 10:03:45 +0300

    2023

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,5 @@ extern crate aoc_runner_derive;
 extern crate crypto;
 
 pub mod day01;
-pub mod day02;
-pub mod day03;
-pub mod day04;
-pub mod day05;
-pub mod day06;
-pub mod day07;
-pub mod day08;
-pub mod day09;
-pub mod day10;
-pub mod day11;
-pub mod day12;
-pub mod day13;
 
-aoc_lib! { year = 2022 }
+aoc_lib! { year = 2023 }
