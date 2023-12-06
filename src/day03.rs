bea9ef8a319a8e8a79313225fc8c7a0999ecfa0a
MrPickles <kirill2013popov@gmail.com>
2023-12-06 10:45:55 +0300

    day3 finally done

--- src/day03.rs
+++ src/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Part {
     pub start: (u32, u32),
@@ -122,6 +124,101 @@ pub fn part1(input: &(Vec<Part>, String)) -> u32 {
     result
 }
 
+fn check_2(c: &char) -> bool {
+    match c {
+        '*' => true,
+        _ => false,
+    }
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &(Vec<Part>, String)) -> u32 {
+    let map: Vec<Vec<char>> = input.1.lines().map(|l| l.chars().collect()).collect();
+    let mut geared: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut result = 0;
+    for part in input.0.clone() {
+        if part.start.0 > 0 {
+            let y = part.start.1;
+            let x = part.start.0 - 1;
+            let row = map
+                .get(y as usize)
+                .expect(format!("expected row at {y}").as_str());
+            let c = row
+                .get(x as usize)
+                .expect(format!("expected char at {x}").as_str());
+            if check_2(c) {
+                if let Some(a) = geared.get(&(x, y)) {
+                    result += part.value * a;
+                } else {
+                    geared.insert((x, y), part.value);
+                }
+                continue;
+            }
+        }
+        if part.start.0 + part.lenght < map.first().unwrap().len() as u32 {
+            let x = part.start.0 + part.lenght;
+            let y = part.start.1;
+            let row = map
+                .get(y as usize)
+                .expect(format!("expected row at {y}").as_str());
+            let c = row
+                .get(x as usize)
+                .expect(format!("expected char at {x}").as_str());
+            if check_2(c) {
+                if let Some(a) = geared.get(&(x, y)) {
+                    result += part.value * a;
+                } else {
+                    geared.insert((x, y), part.value);
+                }
+                continue;
+            }
+        }
+        'a: for x in part.start.0 as i32 - 1..(part.start.0 + part.lenght) as i32 + 1 {
+            if x < 0 || x >= map.first().unwrap().len() as i32 {
+                continue;
+            }
+            let y = part.start.1 as i32 - 1;
+            if y >= 0 {
+                let row = map
+                    .get(y as usize)
+                    .expect(format!("expected row at {y}").as_str());
+                let c = row
+                    .get(x as usize)
+                    .expect(format!("expected char at {x}").as_str());
+                if check_2(c) {
+                    let x = x as u32;
+                    let y = y as u32;
+                    if let Some(a) = geared.get(&(x, y)) {
+                        result += part.value * a;
+                    } else {
+                        geared.insert((x, y), part.value);
+                    }
+                    break 'a;
+                }
+            }
+            let y = part.start.1 + 1;
+            if (y as usize) < map.len() {
+                let row = map
+                    .get(y as usize)
+                    .expect(format!("expected row at {y}").as_str());
+                let c = row
+                    .get(x as usize)
+                    .expect(format!("expected char at {x}").as_str());
+                if check(c) {
+                    let x = x as u32;
+                    if let Some(a) = geared.get(&(x, y)) {
+                        result += part.value * a;
+                    } else {
+                        geared.insert((x, y), part.value);
+                    }
+                    break 'a;
+                }
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -139,5 +236,20 @@ mod tests {
 ...$.*....
 .664.598..";
         assert_eq!(4361, part1(&input_generator(input)));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        assert_eq!(467835, part2(&input_generator(input)));
     }
 }
