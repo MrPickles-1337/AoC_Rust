a28939d4309c53f08b32630e860b0ae8af2b65d9
MrPickles <kirill2013popov@gmail.com>
2023-12-07 15:30:56 +0300

    day5 complete p2 is crazy

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate aoc_runner;
+extern crate rayon;
 
 #[macro_use]
 extern crate aoc_runner_derive;
@@ -7,5 +8,6 @@ pub mod day01;
 pub mod day02;
 pub mod day03;
 pub mod day04;
+pub mod day05;
 
 aoc_lib! { year = 2023 }
