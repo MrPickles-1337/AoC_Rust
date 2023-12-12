61aa74c63e27f637ade0d486ab13f1528931dd79
MrPickles <kirill2013popov@gmail.com>
2023-12-12 12:49:43 +0300

    day6 complete ez

--- src/day06.rs
+++ src/day06.rs
@@ -1,13 +1,13 @@
 use regex::Regex;
 
 #[aoc_generator(day6)]
-pub fn input_generator(input: &str) -> Vec<(u32, u32)> {
+pub fn input_generator(input: &str) -> Vec<(u64, u64)> {
     let rx = Regex::new(r"\d+").unwrap();
-    let i: Vec<Vec<u32>> = input
+    let i: Vec<Vec<u64>> = input
         .lines()
         .map(|l| {
             rx.find_iter(l)
-                .map(|x| x.as_str().parse::<u32>().unwrap())
+                .map(|x| x.as_str().parse::<u64>().unwrap())
                 .collect()
         })
         .collect();
@@ -18,14 +18,39 @@ pub fn input_generator(input: &str) -> Vec<(u32, u32)> {
 }
 
 #[aoc(day6, part1)]
-pub fn part1(input: &Vec<(u32, u32)>) -> u32 {
+pub fn part1(input: &Vec<(u64, u64)>) -> u64 {
     input
         .iter()
         .map(|&(time, distance)| {
-            let yep = 2;
-            2
+            (0..time)
+                .map(|hold_time /* speed */| {
+                    let travel_time = time - hold_time;
+                    let traveled_distance = travel_time * hold_time;
+                    traveled_distance > distance
+                })
+                .filter(|&a| a)
+                .count() as u64
         })
         .product()
+}
+
+#[aoc(day6, part2)]
+pub fn part2(input: &Vec<(u64, u64)>) -> u64 {
+    let mut time = 0;
+    let mut distance = 0;
+    for (t, d) in input.iter() {
+        time = time * 10u64.pow(t.to_string().len() as u32) + t;
+        distance = distance * 10u64.pow(d.to_string().len() as u32) + d;
+    }
+
+    (0..time)
+        .map(|hold_time /* speed */| {
+            let travel_time = time - hold_time;
+            let traveled_distance = travel_time * hold_time;
+            traveled_distance > distance
+        })
+        .filter(|&a| a)
+        .count() as u64
 }
 
 #[cfg(test)]
@@ -37,5 +62,12 @@ mod tests {
         let input = "Time:      7  15   30
 Distance:  9  40  200";
         assert_eq!(288, part1(&input_generator(input)));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = "Time:      7  15   30
+Distance:  9  40  200";
+        assert_eq!(71503, part2(&input_generator(input)));
     }
 }
