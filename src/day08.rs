1be684a192685171687ae138b200fa357a8b6d0e
MrPickles <kirill2013popov@gmail.com>
2023-12-14 14:36:03 +0300

    part2 doesn't want to work

--- src/day08.rs
+++ src/day08.rs
@@ -7,14 +7,7 @@ pub enum Direction {
 }
 
 #[aoc_generator(day8)]
-pub fn input_generator(
-    input: &str,
-) -> (
-    String,
-    String,
-    Vec<Direction>,
-    HashMap<String, (String, String)>,
-) {
+pub fn input_generator(input: &str) -> (Vec<Direction>, HashMap<String, (String, String)>) {
     let mut spl = input.split("\n\n");
     let instructions = spl
         .next()
@@ -43,23 +36,7 @@ pub fn input_generator(
             ),
         );
     });
-    let first = connections_str
-        .lines()
-        .next()
-        .unwrap()
-        .split(' ')
-        .next()
-        .unwrap()
-        .to_string();
-    let last = connections_str
-        .lines()
-        .next_back()
-        .unwrap()
-        .split(' ')
-        .next()
-        .unwrap()
-        .to_string();
-    (first, last, instructions, connections)
+    (instructions, connections)
 }
 
 fn step<'a>(
@@ -94,19 +71,34 @@ fn step<'a>(
     }
 }
 
+fn step_part2<'a>(
+    pos: &'a String,
+    directions: &[Direction],
+    connections: &'a HashMap<String, (String, String)>,
+) -> (bool, u32, &'a String) {
+    if directions.is_empty() {
+        return (false, 0, pos);
+    }
+    let direction = directions.first().unwrap();
+    let next = match direction {
+        Direction::Right => &connections.get(pos).unwrap().1,
+        Direction::Left => &connections.get(pos).unwrap().0,
+    };
+
+    if next.ends_with('Z') {
+        return (true, 1, pos);
+    } else {
+        let (a, b, pos) = step_part2(next, &directions[1..], connections);
+        return (a, b + 1, pos);
+    }
+}
+
 #[aoc(day8, part1)]
-pub fn part1(
-    input: &(
-        String,
-        String,
-        Vec<Direction>,
-        HashMap<String, (String, String)>,
-    ),
-) -> u32 {
+pub fn part1(input: &(Vec<Direction>, HashMap<String, (String, String)>)) -> u32 {
     let mut result = 0;
-    let mut pos = &input.0;
+    let mut pos = &String::from("AAA");
     loop {
-        let (found, steps, last_pos) = step(pos, &input.1, &input.2, &input.3);
+        let (found, steps, last_pos) = step(pos, &"ZZZ".to_string(), &input.0, &input.1);
         if found {
             return result + steps;
         } else {
@@ -114,6 +106,46 @@ pub fn part1(
             pos = last_pos;
         }
     }
+}
+
+#[aoc(day8, part2)]
+pub fn part2(input: &(Vec<Direction>, HashMap<String, (String, String)>)) -> u32 {
+    let mut yep: Vec<u32> = input
+        .1
+        .iter()
+        .filter(|node| node.0.ends_with('A'))
+        .map(|a| a.0)
+        .map(|node| {
+            let mut result = 0;
+            let mut pos = node;
+            loop {
+                let (found, steps, last_pos) = step_part2(pos, &input.0, &input.1);
+                if found {
+                    return result + steps;
+                } else {
+                    result += steps;
+                    pos = last_pos;
+                }
+            }
+        })
+        .collect();
+    yep.sort();
+    lcm(&yep)
+}
+
+fn lcm(nums: &[u32]) -> u32 {
+    let mut result = 1;
+    for &num in nums {
+        result = num * result / gcd(num, result);
+    }
+    result
+}
+
+fn gcd(a: u32, b: u32) -> u32 {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
 }
 
 #[cfg(test)]
@@ -138,5 +170,20 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
         assert_eq!(2, part1(&input_generator(input1)));
         assert_eq!(6, part1(&input_generator(input2)));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+        assert_eq!(6, part2(&input_generator(input)));
     }
 }
