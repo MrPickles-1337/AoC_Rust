d60ad3cd0031470b83fc4deb8e7fc046f2bda118
MrPickles <kirill2013popov@gmail.com>
2025-03-25 15:34:56 +0300

    rename

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ extern crate aoc_runner;
 extern crate aoc_runner_derive;
 extern crate crypto;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
 
 aoc_lib! { year = 2015 }
