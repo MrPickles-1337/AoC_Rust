c7536e8f2f643736bbf8432b4c621faf372eddc0
MrPickles <kirill2013popov@gmail.com>
2025-06-11 13:44:27 +0300

    day11 wip

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[aoc_generator(day11)]
 pub fn input_generator(input: &str) -> Vec<usize> {
     input.split(' ').map(|i| i.parse().unwrap()).collect()
@@ -24,23 +26,48 @@ fn process(a: &mut Vec<usize>, b: &mut Vec<usize>) {
 #[aoc(day11, part1)]
 pub fn part1(input: &[usize]) -> usize {
     let mut a = Vec::from(input);
-    let mut b = Vec::new();
     for _ in 0..25 {
+        let mut b = Vec::new();
         process(&mut a, &mut b);
-        std::mem::swap(&mut a, &mut b);
+        a = b;
     }
-    a.len() + b.len()
+    a.len()
+}
+
+pub fn blink_stone(i: usize, blinks: u32) -> Vec<usize> {
+    let mut a = vec![i];
+    for i in 0..blinks {
+        println!("blink {i}");
+        let mut b = Vec::new();
+        process(&mut a, &mut b);
+        a = b;
+    }
+    a
+}
+
+pub fn wink_stone(i: usize, blinks: u32, cache: &mut HashMap<(usize, u32), usize>) -> usize {
+    eprintln!("cache size: {}", cache.len());
+    println!("wink: {blinks}");
+    if blinks == 0 {
+        return 1;
+    }
+    if let Some(e) = cache.get(&(i, blinks)) {
+        return *e;
+    }
+    let mut total = 0;
+    let stones = blink_stone(i, blinks);
+    for s in stones {
+        total += wink_stone(s, blinks - 1, cache);
+    }
+
+    cache.insert((i, blinks), total);
+    total
 }
 
 #[aoc(day11, part2)]
 pub fn part2(input: &[usize]) -> usize {
-    let mut a = Vec::from(input);
-    let mut b = Vec::new();
-    for _ in 0..75 {
-        process(&mut a, &mut b);
-        std::mem::swap(&mut a, &mut b);
-    }
-    a.len() + b.len()
+    let mut cache = HashMap::new();
+    input.iter().map(|i| wink_stone(*i, 75, &mut cache)).sum()
 }
 
 #[cfg(test)]
@@ -52,5 +79,12 @@ mod tests {
         let input = "125 17";
 
         assert_eq!(55312, part1(&input_generator(input)));
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = "125 17";
+
+        assert_eq!(55312, part2(&input_generator(input)));
     }
 }
